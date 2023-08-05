b1ce78b4d57527c8272b4032e1a762584d8c8b0c
Olga Mirishnichenko <ollf72@mail.ru>
2023-08-05 19:18:07 +0500

    sprint_3

---
+++ locators.py
@@ -0,0 +1,25 @@
+
+BUTTON_SING_IN = '//button[text()="Войти в аккаунт"]'
+REGISTRATION = '//a[@href="/register"]'
+REG_NAME = '//label[.="Имя"]/following-sibling::input'
+REG_EMAIL = '//label[.="Email"]/following-sibling::input'
+REG_PASSWORD = '//input[@type="password"]'
+BUTTON_REG = '//button[text()="Зарегистрироваться"]'
+BUTTON_ACCOUNT = '//a[@href="/account"]' # кнопка личный кабинет
+BUTTON_LOG_IN = '//button[text()="Войти"]'# кнопка ВОЙТИ
+
+FORM_NAME_REGIS='//h2[text()="Регистрация"]' #название формы регистрации
+ACCOUNT_ORDER_HISTORY = '//a[@href="/account/order-history"]' #профиль
+ACCOUNT_PROFILE = '//a[@href="/account/profile"]'
+BURGER_INGR = '//h1[text()="Соберите бургер"]' #собери бургер
+BUTTON_RESTORE = '//button[text()="Восстановить"]'# кнопка Восстановить
+FORGOT_PASSW = '//a[@href="/forgot-password"]' # Восстановить пароль
+BUTTON_SAVE = '//button[text()="Сохранить"]'#кнопка Сохранить
+BUTTON_EXIT = '//button[text()="Выход"]' #кнопка Выход
+SECTION_SAUCES = '//span[text()="Соусы"]/parent::div'
+SECTION_TOPPINGS = '//span[text()="Начинки"]/parent::div'
+SECTION_BUN = '//span[text()="Булки"]/parent::div'
+NAME_LOGIN_FORM = '//h2[text()="Вход"]' #название формы Вход
+LINK_TEXT_CONSTR = '//a[@href="/"]'
+LOGO = 'AppHeader_header__logo__2D0X2'
+BUTTON_ORDER_FEED = '//p[text()="Лента Заказов"]'
